c65306dc20533b178810645476210544f8fc58a7
W3LabKr <w3labkr@gmail.com>
2025-02-09 22:22:42 +0900

    Add environment variables

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -8,7 +8,19 @@ declare global {
 
       OPENAI_API_KEY: string
       ASSISTANT_ID: string
+      AZURE_RESOURCE_NAME: string
+      AZURE_API_KEY: string
+      AWS_ACCESS_KEY_ID: string
+      AWS_SECRET_ACCESS_KEY: string
+      AWS_REGION: string
+      GOOGLE_GENERATIVE_AI_API_KEY: string
+      GOOGLE_VERTEX_PROJECT: string
+      GOOGLE_VERTEX_LOCATION: string
+      GOOGLE_APPLICATION_CREDENTIALS: string
+      ANTHROPIC_API_KEY: string
       GROQ_API_KEY: string
+      FIREWORKS_API_KEY: string
+      COHERE_API_KEY: string
 
       [key: string]: string | undefined
     }
