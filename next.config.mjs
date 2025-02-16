069aceef90a727ea88cac89ec9e1599ae7c33f9c
W3LabKr <w3labkr@gmail.com>
2025-02-16 14:51:50 +0900

    fix: Unrecognized key(s) in object: 'reactStrictMode' at "eslint"

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
+// @ts-check
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Suggested: We strongly suggest you enable Strict Mode in your Next.js application to better prepare your application for the future of React.
+  reactStrictMode: process.env.NODE_ENV === 'production',
   eslint: {
-    // Suggested: We strongly suggest you enable Strict Mode in your Next.js application to better prepare your application for the future of React.
-    reactStrictMode: process.env.NODE_ENV === 'production',
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
